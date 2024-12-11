65c8061f9a5c1f3f76392c55eaaacb55733432e9
Jyxarthur <92585830+Jyxarthur@users.noreply.github.com>
2024-12-12 05:52:58 +0700

    * Enable amodal mask to modal mask conversion within OCLR dataset

--- data/dataset_utils.py
+++ data/dataset_utils.py
@@ -52,6 +52,50 @@ def processMultiSeg(sample_dir, gt_resolution = None, out_channels = 10, dataset
     return masks_float
 
 
+def processMultiAmodalSeg(sample_dir, gt_resolution = None, out_channels = 10, dataset = 'oclr', with_bg = False):
+    """ 
+    Input: Amodal segmentation path of OCLR synthetic data
+    Output: Model segmentation
+    """
+    basename = os.path.basename(sample_dir)
+    dirname = os.path.dirname(sample_dir)
+    catname = os.path.basename(dirname)
+    masks = []
+    for i in range(1, out_channels + 1):
+        if i <= int(catname[0]):
+            img = cv2.imread(os.path.join(dirname, str(i) + '_' + basename))
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            gt = img / 255.
+            gt = (gt > 0.5).astype(np.float32)
+            masks.append(gt)
+        else:
+            masks.append(gt * 0.)  
+    
+    # Overlaying amodal masks to masks modal masks
+    recon_masks = np.zeros_like(masks)
+    acc_mask = np.zeros_like(masks)[0]
+    for channel in reversed(range(len(masks))):
+        recon_masks[channel] = masks[channel] * (1 - acc_mask) 
+        acc_mask = (1 - acc_mask) * masks[channel] + acc_mask
+    
+    # Add an empty background channel to the front
+    recon_masks = [recon_masks[0] * 0] + [recon_masks[i] for i in range(recon_masks.shape[0])]
+    recon_masks = recon_masks[0 : out_channels]
+    
+    masks_float = []
+    for i, mask in enumerate(recon_masks):
+        if gt_resolution is not None:
+            mask_float = (cv2.resize(mask, (gt_resolution[1], gt_resolution[0]), interpolation=cv2.INTER_LINEAR) > 0.5).astype(np.float32)
+        else:
+            mask_float = mask
+        masks_float.append(mask_float)
+    if with_bg:
+        masks_float = np.stack(masks_float, 0)[:, :, :, 0]
+    else:
+        masks_float = np.stack(masks_float, 0)[1:, :, :, 0]
+
+    return masks_float
+
 def preprocess(x: torch.Tensor) -> torch.Tensor:
     """Normalize pixel values and pad to a square input."""
     # Normalize colors
